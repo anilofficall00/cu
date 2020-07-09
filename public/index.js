14d21153ba2f7c7e4fe7c049bd009e4b0d6831ac
Glitch (heavy-rune-yttrium) <none>
2020-07-09 20:59:08 +0000

    🎸🐌 Checkpoint ./public/index.html:14176681/8 ./public/index.js:14176681/692

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@
 // Login & Fetch Data
 var socket = io();
 var bearer = getCookie("bearer");
-if (!bearer) updateLoadingHTML('<a href="https://discord.com/api/oauth2/authorize?client_id=730883344757162046&redirect_uri=https%3A%2F%2Fadvancedbot101-manager.glitch.me%2Fcallback&response_type=code&scope=identify"><button type="button" class="btn btn-dark">Login With Discord</button></a>')
+if (!bearer) updateLoadingHTML('<a href="https://discord.com/api/oauth2/authorize?client_id=730883344757162046&redirect_uri=https%3A%2F%2Fapi.advancedbot101.glitch.me%2Fapi%2Fv1%2Fauth%2Fcallback&response_type=code&scope=identify"><button type="button" class="btn btn-dark">Login With Discord</button></a>')
 else socket.emit('getInfo', bearer);
 
 function updateLoadingHTML(html) {
@@ -32,7 +32,7 @@ socket.on('getNewInfo', function() {
 socket.on('getInfo', function(info) {
 
   // Verify Login & Access
-  if (!info) updateLoadingHTML('<a href="https://discord.com/api/oauth2/authorize?client_id=730883344757162046&redirect_uri=https%3A%2F%2F2Fadvancedbot101-manager.glitch.me%2Fcallback&response_type=code&scope=identify"><button type="button" class="btn btn-dark">Login With Discord</button></a>')
+  if (!info) updateLoadingHTML('<a href="https://discord.com/api/oauth2/authorize?client_id=730883344757162046&redirect_uri=https%3A%2F%2Fapi.advancedbot101.glitch.me%2Fapi%2Fv1%2Fauth%2Fcallback&response_type=code&scope=identify"><button type="button" class="btn btn-dark">Login With Discord</button></a>')
   else if (info === 403) updateLoadingHTML('You do not have access to view this pannel, Try again ');
   else window.loading_screen.finish();
   
